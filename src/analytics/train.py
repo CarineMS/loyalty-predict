9869e65b13d405ad3ddb6dbbc5950e30ec9e693d
CarineMS <carinemadsoares@gmail.com>
2026-07-27 20:10:48 -0300

    :broom: feat: Analise exploratoria de valores faltantes

--- src/analytics/train.py
+++ src/analytics/train.py
@@ -10,6 +10,7 @@ conn = sqlalchemy.create_engine("sqlite:///../../data/analytics/database.db")
 
 # SAMPLE - IMPORT DOS DADOS
 df = pd.read_sql("abt_fiel", conn)
+df.head()
 
 # %%
 
@@ -32,7 +33,19 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(
     X, y,
     test_size=0.2,
     random_state=42,
+    stratify=y,
 )
 
 print(f"Base Treino: {y_train.shape[0]} Unid. | Tx. Target {100*y_train.mean():.2f}%")
 print(f"Base Teste: {y_test.shape[0]} Unid. | Tx. Target {100*y_test.mean():.2f}%")
+
+# %%
+
+# EXPLORE - MISSING
+
+s_nas = X_train.isna().mean()
+s_nas = s_nas[s_nas>0]
+s_nas
+
+# %%
+df[df['descLifeCycleAtual'].isna()]
